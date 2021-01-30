c10be8fc95d61b089f112b466ca8e9bfaaff9c73
Ilya Kushlianski <kushliansky@gmail.com>
2021-01-31 01:47:57 +0300

    experiment with IOContainer

---
+++ src/ioc-inversify/index.ts
@@ -0,0 +1,8 @@
+import container from "./inversify.config";
+import TYPES from "./types";
+import {Warrior} from "./interfaces";
+
+const ninja = container.get<Warrior>(TYPES.Warrior);
+
+console.log(ninja.fight())
+console.log(ninja.sneak())
---
+++ src/ioc-inversify/inversify.config.ts
@@ -0,0 +1,12 @@
+import { Container } from "inversify";
+
+import TYPES from "./types";
+import { Ninja, Katana, Shuriken } from "./classes";
+import {ThrowableWeapon, Warrior, Weapon} from "./interfaces";
+
+const container = new Container();
+container.bind<Warrior>(TYPES.Warrior).to(Ninja);
+container.bind<Weapon>(TYPES.Weapon).to(Katana);
+container.bind<ThrowableWeapon>(TYPES.ThrowableWeapon).to(Shuriken);
+
+export default container;
