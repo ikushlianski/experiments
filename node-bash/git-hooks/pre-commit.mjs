8e783d554a5150e3b314fb85faa423367c119c89
Ilya Kushlianski <kushliansky@gmail.com>
2022-10-08 15:27:14 +0400

    Pre-commit with test based on Node.js, not bash
    Signed-off-by: Ilya Kushlianski <kushliansky@gmail.com>

--- node-bash/git-hooks/pre-commit.mjs
+++ node-bash/git-hooks/pre-commit.mjs
@@ -2,18 +2,9 @@
 
 import { exec } from 'node:child_process'
 
-// import * as os from 'node:os';
-//
-// const {username} = os.userInfo();
-// console.log(`Hello ${username}`);
-
 exec('npm run test', (error, stdout, stderr) => {
   if (error) {
     console.error(`exec error: ${error}`);
     throw new Error('tests failed!')
   }
-
-  console.log(`stderr: ${stderr}`);
-
-
 });
