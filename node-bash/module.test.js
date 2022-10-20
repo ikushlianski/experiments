9dc6dab094c1b5bd09322fc006c14f21b0add8f1
Ilya Kushlianski <kushliansky@gmail.com>
2022-10-20 13:43:27 +0400

    Add subtract tests, add windows compatibility
    Signed-off-by: Ilya Kushlianski <kushliansky@gmail.com>

--- node-bash/module.test.js
+++ node-bash/module.test.js
@@ -1,7 +1,11 @@
-const {add} = require('./module')
+const {add, subtract} = require('./module')
 
 describe('Module test', () => {
   it('should output 5 for arguments 2 and 3', () => {
     expect(add(2,3)).toBe(5)
+  });
+
+  it('should subtract 15 from 25 and output 10', () => {
+    expect(subtract(25,15)).toBe(10)
   });
 });
