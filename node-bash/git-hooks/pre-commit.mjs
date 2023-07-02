7e0d9463452906ee7b9e1c637ae4640a780a2f4a
Ilya Kushlianski <kushliansky@gmail.com>
2023-07-02 14:12:01 +0400

    Add script removing all node_modules folders from here
    Signed-off-by: Ilya Kushlianski <kushliansky@gmail.com>

--- node-bash/git-hooks/pre-commit.mjs
+++ node-bash/git-hooks/pre-commit.mjs
@@ -2,24 +2,23 @@
 
 import { exec } from 'node:child_process'
 
-exec('npm run test', (error, stdout, stderr) => {
-  console.log(stdout)
-
-  if (error) {
-    console.error(`exec error: ${error}`);
-    throw new Error('tests failed!')
-  }
-
-  console.log('All tests passed\n\n');
-});
+// exec('npm run test', (error, stdout, stderr) => {
+//   console.log(stdout)
+//
+//   if (error) {
+//     console.error(`exec error: ${error}`);
+//     throw new Error('tests failed!')
+//   }
+//
+//   console.log('All tests passed\n\n');
+// });
 
 exec('git diff --diff-filter=d --name-only --cached | grep -E \'\\.(mjs|js|jsx|ts|tsx)$\'', (error, stdout, stderr) => {
   if (error) {
-    console.error(`exec error: ${error}`);
-    throw new Error('could not get diff!')
+    console.warn(`exec warning: ${error}`);
+  } else {
+    const files = stdout.split('\n').filter(file => file)
+
+    console.log('files', files)
   }
-
-  const files = stdout.split('\n').filter(file => file)
-
-  console.log('files', files)
 });
